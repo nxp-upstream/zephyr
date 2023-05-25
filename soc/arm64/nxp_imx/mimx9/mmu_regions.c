4d41376a671d7d0abd7506882afdb23c87d4c7d6
Chekhov Ma <chekhov.ma@nxp.com>
2023-05-25 17:03:04 +0800

    imx93: enable rgpio driver on i.mx93
    Add HAS_MCUX_RGPIO to Kconfig.soc
    Add gpio1 ~ gpio4 dts node and pinctrl node in nxp_mimx93_a55.dtsi
    Add GPIO1 ~ GPIO4 MMU region in mmu_regions.c
    
    Signed-off-by: Chekhov Ma <chekhov.ma@nxp.com>

--- soc/arm64/nxp_imx/mimx9/mmu_regions.c
+++ soc/arm64/nxp_imx/mimx9/mmu_regions.c
@@ -45,6 +45,26 @@ static const struct arm_mmu_region mmu_regions[] = {
 			      DT_REG_SIZE(DT_NODELABEL(iomuxc)),
 			      MT_DEVICE_nGnRnE | MT_P_RW_U_NA | MT_NS),
 
+	MMU_REGION_FLAT_ENTRY("GPIO1",
+			      DT_REG_ADDR(DT_NODELABEL(gpio1)),
+			      DT_REG_SIZE(DT_NODELABEL(gpio1)),
+			      MT_DEVICE_nGnRnE | MT_P_RW_U_NA | MT_NS),
+
+	MMU_REGION_FLAT_ENTRY("GPIO2",
+			      DT_REG_ADDR(DT_NODELABEL(gpio2)),
+			      DT_REG_SIZE(DT_NODELABEL(gpio2)),
+			      MT_DEVICE_nGnRnE | MT_P_RW_U_NA | MT_NS),
+
+	MMU_REGION_FLAT_ENTRY("GPIO3",
+			      DT_REG_ADDR(DT_NODELABEL(gpio3)),
+			      DT_REG_SIZE(DT_NODELABEL(gpio3)),
+			      MT_DEVICE_nGnRnE | MT_P_RW_U_NA | MT_NS),
+
+	MMU_REGION_FLAT_ENTRY("GPIO4",
+			      DT_REG_ADDR(DT_NODELABEL(gpio4)),
+			      DT_REG_SIZE(DT_NODELABEL(gpio4)),
+			      MT_DEVICE_nGnRnE | MT_P_RW_U_NA | MT_NS),
+
 #if CONFIG_SOF
 	MMU_REGION_FLAT_ENTRY("MU2_A",
 			      DT_REG_ADDR(DT_NODELABEL(mu2_a)),
