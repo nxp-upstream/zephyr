b4ccd720af58ea21dfefd0a0895999086838f301
Daniel DeGrasse <daniel.degrasse@nxp.com>
2024-03-30 01:52:35 -0500

    wip: got first basic syscon driver to compile
    Now have syscon source driver compiling based on devicetree clock nodes
    
    Signed-off-by: Daniel DeGrasse <daniel.degrasse@nxp.com>

---
+++ drivers/clock_mgmt/nxp_syscon/nxp_syscon_source.c
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2024 NXP
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/drivers/clock_mgmt/clock_driver.h>
+
+#define DT_DRV_COMPAT nxp_syscon_clock_source
+
+struct syscon_clock_source_config {
+	uint32_t rate;
+	volatile uint32_t *reg;
+	uint8_t enable_offset;
+};
+
+int syscon_clock_source_get_rate(const struct clk *clk)
+{
+	const struct syscon_clock_source_config *config = clk->config;
+
+	if (config->reg == NULL) {
+		return config->rate;
+	}
+
+	return ((*config->reg) & BIT(config->enable_offset)) ?
+		config->rate : 0;
+}
+
+int syscon_clock_source_configure(const struct clk *clk, void *data)
+{
+	const struct syscon_clock_source_config *config = clk->config;
+	bool ungate = (bool)data;
+
+	if (config->reg == NULL) {
+		return 0;
+	}
+
+	if (ungate) {
+		(*config->reg) |= BIT(config->enable_offset);
+	} else {
+		(*config->reg) &= ~BIT(config->enable_offset);
+	}
+	return 0;
+}
+
+void syscon_clock_source_init(const struct clk *clk)
+{
+	/* Nothing here yet */
+}
+
+const struct clock_driver_api nxp_syscon_source_api = {
+	.get_rate = syscon_clock_source_get_rate,
+	.configure = syscon_clock_source_configure,
+};
+
+#define NXP_SYSCON_CLOCK_DEFINE(inst)                                          \
+	const struct syscon_clock_source_config nxp_syscon_source_##inst = {   \
+		.rate = DT_INST_PROP(inst, frequency),                         \
+		.reg = (volatile uint32_t *)DT_INST_REG_ADDR(inst),            \
+		.enable_offset = (uint8_t)DT_INST_PROP(inst, offset),          \
+	};                                                                     \
+	                                                                       \
+	CLOCK_DT_INST_DEFINE(inst, syscon_clock_source_init, NULL,             \
+			     &nxp_syscon_source_##inst,                        \
+			     &nxp_syscon_source_api);
+
+DT_INST_FOREACH_STATUS_OKAY(NXP_SYSCON_CLOCK_DEFINE)
--- include/zephyr/drivers/clock_mgmt/clock_driver.h
+++ include/zephyr/drivers/clock_mgmt/clock_driver.h
@@ -12,8 +12,8 @@
 #ifndef ZEPHYR_INCLUDE_DRIVERS_CLOCK_MGMT_CLOCK_DRIVER_H_
 #define ZEPHYR_INCLUDE_DRIVERS_CLOCK_MGMT_CLOCK_DRIVER_H_
 
-#include <zephyr/device.h>
 #include <zephyr/sys/slist.h>
+#include <zephyr/device.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -31,22 +31,22 @@ extern "C" {
  */
 struct clk {
 	/** Callbacks to notify when clock is reconfigured */
-	sys_slist_t callbacks
+	sys_slist_t callbacks;
 	/** Address of private clock instance configuration information */
 	const void *config;
 	/** Address of private clock instance mutable data */
 	void *data;
 	/** Gets clock rate in Hz */
-	int (*get_rate)(struct clk*);
+	int (*get_rate)(const struct clk *clk);
 	/** Configure a clock with device specific data */
-	int (*configure)(struct clk*, void*);
+	int (*configure)(const struct clk *clk, void *data);
 #ifdef CONFIG_CLOCK_MGMT_SET_SET_RATE
 	/** Gets nearest rate clock can support, in Hz */
-	int (*round_rate)(struct clk*, uint32_t);
+	int (*round_rate)(const struct clk *clk, uint32_t rate);
 	/** Sets clock rate in Hz */
-	int (*set_rate)(struct clk*, uint32_t);
+	int (*set_rate)(const struct clk *clk, uint32_t rate);
 #endif
-}
+};
 
 /**
  * @brief Clock Driver API
@@ -56,14 +56,70 @@ struct clk {
  */
 struct clock_driver_api {
 	/** Gets clock rate in Hz */
-	int (*get_rate)(struct clk*);
+	int (*get_rate)(const struct clk *clk);
 	/** Configure a clock with device specific data */
-	int (*configure)(struct clk*, void*);
+	int (*configure)(const struct clk *clk, void *data);
 	/** Gets nearest rate clock can support, in Hz */
-	int (*round_rate)(struct clk*, uint32_t);
+	int (*round_rate)(const struct clk *clk, uint32_t rate);
 	/** Sets clock rate in Hz */
-	int (*set_rate)(struct clk*, uint32_t);
+	int (*set_rate)(const struct clk *clk, uint32_t rate);
 };
+
+/**
+ * @brief Clock Driver initialization structure
+ */
+struct clock_init {
+	/** Clock initialization function */
+	void (*init_fn)(const struct clk *clk);
+	/** Parameter to pass to initialization function*/
+	const struct clk *clk;
+};
+
+/**
+ * @brief Expands to the name of a global clock object.
+ *
+ * Return the full name of a clock object symbol created by CLOCK_DT_DEFINE(),
+ * using the `dev_id` provided by Z_DEVICE_DT_DEV_ID(). This is the name of the
+ * global variable storing the clock structure
+ *
+ * It is meant to be used for declaring extern symbols pointing to clock objects
+ * before using the CLOCK_GET macro to get the device object.
+ *
+ * @param dev_id Device identifier.
+ *
+ * @return The full name of the clock object defined by clock definition
+ * macros.
+ */
+#define CLOCK_NAME_GET(dev_id) _CONCAT(__clock_, dev_id)
+
+/**
+ * @brief The name of the global clock object for @param node_id
+ *
+ * Returns the name of the global clock structure as a C identifier. The clock
+ * must be allocated using CLOCK_DT_DEFINE() or CLOCK_DT_INST_DEFINE() for
+ * this to work.
+ *
+ * @param node_id Devicetree node identifier
+ *
+ * @return The name of the clock object as a C identifier
+ */
+#define CLOCK_DT_NAME_GET(node_id) CLOCK_NAME_GET(Z_DEVICE_DT_DEV_ID(node_id))
+
+/**
+ * @brief Get a @ref clk reference from a clock devicetree node identifier.
+ *
+ * Returns a pointer to a clock object created from a devicetree node, if any
+ * clock was allocated by a driver. If not such clock was allocated, this will
+ * fail at linker time. If you get an error that looks like
+ * `undefined reference to __device_dts_ord_<N>`, that is what happened.
+ * Check to make sure your clock driver is being compiled,
+ * usually by enabling the Kconfig options it requires.
+ *
+ * @param node_id A devicetree node identifier
+ *
+ * @return A pointer to the clock object created for that node
+ */
+#define CLOCK_DT_GET(node_id) (&CLOCK_DT_NAME_GET(node_id))
 
 /**
  * @brief Initializer for @ref clk.
@@ -78,11 +134,11 @@ struct clock_driver_api {
 		.callbacks = SYS_SLIST_STATIC_INIT(_callbacks),                \
 		.data = data_,                                                 \
 		.config = config_,                                             \
-		.get_rate = api.get_rate,                                      \
-		.configure = api.configure,                                    \
+		.get_rate = (api)->get_rate,                                   \
+		.configure = (api)->configure,                                 \
 		IF_ENABLED(CONFIG_CLOCK_MGMT_SET_SET_RATE, (                   \
-		.round_rate = api.round_rate,                                  \
-		.set_rate = api.set_rate,))                                    \
+		.round_rate = (api)->round_rate,                               \
+		.set_rate = (api)->set_rate,))                                 \
 	}
 
 /**
@@ -99,8 +155,29 @@ struct clock_driver_api {
  * @param api Pointer to the clock's API structure.
  */
 #define Z_CLOCK_BASE_DEFINE(node_id, clk_id, data, config, api)                \
-	const struct clk DEVICE_NAME_GET(clk_id) =                             \
-		Z_CLOCK_INIT(&(DEVICE_NAME_GET(clk_id).callbacks), data, config)
+	const struct clk CLOCK_NAME_GET(clk_id) =                              \
+		Z_CLOCK_INIT(&(CLOCK_NAME_GET(clk_id).callbacks), data,        \
+			     config, api);
+
+/**
+ * @brief Obtain clock init entry name.
+ *
+ * @param init_id Init entry unique identifier.
+ */
+#define Z_CLOCK_INIT_NAME(init_id) _CONCAT(__clock_init_, CLOCK_NAME_GET(init_id))
+
+/**
+ * @brief Define the init entry for a clock.
+ *
+ * @param node_id Devicetree node id for the clock
+ * @param dev_id Device identifier.
+ * @param init_fn_ Clock init function.
+ */
+#define Z_CLOCK_INIT_ENTRY_DEFINE(node_id, dev_id, init_fn_)                   \
+	struct clock_init Z_CLOCK_INIT_NAME(dev_id) = {                        \
+		.init_fn = init_fn_,                                           \
+		.clk = CLOCK_DT_GET(node_id),                                  \
+	};
 
 /**
  * @brief Create a clock object from a devicetree node identifier and set it
@@ -120,7 +197,8 @@ struct clock_driver_api {
  *
  * @param node_id The devicetree node identifier.
  * @param init_fn Pointer to the clock's initialization function, which will
- * be run by the kernel during system initialization. Can be `NULL`.
+ * be run by the kernel during system initialization. Can be `NULL`. Note
+ * that this function runs in the PRE_KERNEL_1 init phase.
  * @param data Pointer to the clock's private mutable data, which will be
  * stored in the @ref clk.data field
  * @param config Pointer to the clock's private constant data, which will be
@@ -131,7 +209,7 @@ struct clock_driver_api {
 #define CLOCK_DT_DEFINE(node_id, init_fn, data, config, api, ...)              \
 	Z_CLOCK_BASE_DEFINE(node_id, Z_DEVICE_DT_DEV_ID(node_id), data,        \
 			    config, api)                                       \
-	Z_CLOCK_INIT_ENTRY_DEFINE(node_id, dev_id, init_fn)
+	Z_CLOCK_INIT_ENTRY_DEFINE(node_id, Z_DEVICE_DT_DEV_ID(node_id), init_fn)
 
 /**
  * @brief Like CLOCK_DT_DEFINE(), but uses an instance of `DT_DRV_COMPAT`
@@ -145,8 +223,6 @@ struct clock_driver_api {
 
 #ifdef __cplusplus
 }
-#endif
-
 #endif
 
 /**
