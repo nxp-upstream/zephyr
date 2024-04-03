65bcb0cd964e8a19f86065cf9702e14cdbcf16ae
Daniel DeGrasse <daniel.degrasse@nxp.com>
2024-04-03 08:15:01 -0500

    added basic reference to clock_get in hello world
    This is just for testing, to see if the framework actually functions
    
    Signed-off-by: Daniel DeGrasse <daniel.degrasse@nxp.com>

--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -5,9 +5,15 @@
  */
 
 #include <stdio.h>
+#include <zephyr/drivers/clock_mgmt/clock_driver.h>
 
 int main(void)
 {
+#ifdef CONFIG_CLOCK_MGMT
+	const struct clk *clk = CLOCK_DT_GET(DT_NODELABEL(fcclksel0));
+
+	printf("Clock rate was %d\n", clock_get_rate(clk));
+#endif
 	printf("Hello World! %s\n", CONFIG_BOARD_TARGET);
 
 	return 0;
