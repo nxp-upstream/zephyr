8dda07eb0452ebb63f45be8d83296b322cebdcbb
Daniel DeGrasse <daniel.degrasse@nxp.com>
2024-04-03 09:54:27 -0500

    optimized mux to no longer store multiple "no_clock" pointers
    Mux does not need to store pointers for multiple "no_clock" selections
    at end of array, so remove these. Saves about 10 bytes.
    
    Signed-off-by: Daniel DeGrasse <daniel.degrasse@nxp.com>

--- drivers/clock_mgmt/nxp_syscon/nxp_syscon_mux.c
+++ drivers/clock_mgmt/nxp_syscon/nxp_syscon_mux.c
@@ -9,10 +9,11 @@
 #define DT_DRV_COMPAT nxp_syscon_clock_mux
 
 struct syscon_clock_mux_config {
-	const struct clk **parents;
+	const struct clk *const *parents;
 	volatile uint32_t *reg;
-	uint8_t mask_width:3;
-	uint8_t mask_offset:5;
+	uint8_t mask_width;
+	uint8_t mask_offset;
+	uint8_t src_count;
 };
 
 int syscon_clock_mux_get_rate(const struct clk *clk)
@@ -22,6 +23,10 @@ int syscon_clock_mux_get_rate(const struct clk *clk)
 				   (config->mask_width +
 				   config->mask_offset - 1));
 	uint8_t sel = ((*config->reg) & mux_mask) >> config->mask_offset;
+
+	if (sel > config->src_count) {
+		return -EIO;
+	}
 
 	return clock_get_rate(config->parents[sel]);
 }
@@ -35,6 +40,9 @@ int syscon_clock_mux_configure(const struct clk *clk, void *mux)
 				   config->mask_offset - 1));
 	uint32_t mux_val = FIELD_PREP(mux_mask, ((uint32_t)mux));
 
+	if (((uint32_t)mux) > config->src_count) {
+		return -EINVAL;
+	}
 
 	(*config->reg) = ((*config->reg) & ~mux_mask) | mux_val;
 	return 0;
@@ -47,6 +55,10 @@ int syscon_clock_mux_notify(const struct clk *clk, const struct clk *parent)
 				   (config->mask_width +
 				   config->mask_offset - 1));
 	uint8_t sel = ((*config->reg) & mux_mask) >> config->mask_offset;
+
+	if (sel > config->src_count) {
+		return -EINVAL;
+	}
 
 	/*
 	 * Read div reg, and if index matches parent index we should notify
@@ -72,7 +84,7 @@ const struct clock_driver_api nxp_syscon_mux_api = {
 	CLOCK_DT_GET(DT_PHANDLE_BY_IDX(node_id, prop, idx)),
 
 #define NXP_SYSCON_CLOCK_DEFINE(inst)                                          \
-	const struct clk *nxp_syscon_mux_parents_##inst[] = {                  \
+	const struct clk *const nxp_syscon_mux_parents_##inst[] = {            \
 		DT_INST_FOREACH_PROP_ELEM(inst, input_sources, GET_MUX_INPUT)  \
 	};                                                                     \
 	DT_INST_FOREACH_PROP_ELEM_VARGS(inst, input_sources, REG_MUX_CALLBACK, \
@@ -81,6 +93,7 @@ const struct clock_driver_api nxp_syscon_mux_api = {
 	 	.parents = nxp_syscon_mux_parents_##inst,                      \
 		.reg = (volatile uint32_t *)DT_INST_REG_ADDR(inst),            \
 		.mask_width = (uint8_t)DT_INST_REG_SIZE(inst),                 \
+		.src_count = ARRAY_SIZE(nxp_syscon_mux_parents_##inst),        \
 	};                                                                     \
 	                                                                       \
 	CLOCK_DT_INST_DEFINE(inst, NULL,                                       \
