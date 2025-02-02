60378d9b4717c97154ba3e9810b2115d93945df3
Sumit Batra <sumit.batra@nxp.com>
2025-02-03 01:27:08 +0530

    ICM4268X Intermidiate-1
    Signed-off-by: Sumit Batra <sumit.batra@nxp.com>

--- drivers/sensor/tdk/icm4268x/icm4268x_trigger.c
+++ drivers/sensor/tdk/icm4268x/icm4268x_trigger.c
@@ -1,6 +1,7 @@
 /*
  * Copyright (c) 2022 Intel Corporation
  * Copyright (c) 2023 Google LLC
+ * Copyright 2025 NXP
  * SPDX-License-Identifier: Apache-2.0
  */
 
@@ -10,78 +11,78 @@
 #include <zephyr/logging/log.h>
 #include <zephyr/sys/util.h>
 
-#include "icm42688.h"
-#include "icm42688_reg.h"
-#include "icm42688_rtio.h"
-#include "icm42688_spi.h"
-#include "icm42688_trigger.h"
+#include "icm4268x.h"
+#include "icm4268x_reg.h"
+#include "icm4268x_rtio.h"
+#include "icm4268x_spi.h"
+#include "icm4268x_trigger.h"
 
-LOG_MODULE_DECLARE(ICM42688, CONFIG_SENSOR_LOG_LEVEL);
+LOG_MODULE_DECLARE(ICM4268X, CONFIG_SENSOR_LOG_LEVEL);
 
-static void icm42688_gpio_callback(const struct device *dev, struct gpio_callback *cb,
+static void icm4268x_gpio_callback(const struct device *dev, struct gpio_callback *cb,
 				   uint32_t pins)
 {
-	struct icm42688_dev_data *data = CONTAINER_OF(cb, struct icm42688_dev_data, gpio_cb);
+	struct icm4268x_dev_data *data = CONTAINER_OF(cb, struct icm4268x_dev_data, gpio_cb);
 
 	ARG_UNUSED(dev);
 	ARG_UNUSED(pins);
 
-#if defined(CONFIG_ICM42688_TRIGGER_OWN_THREAD)
+#if defined(CONFIG_ICM4268X_TRIGGER_OWN_THREAD)
 	k_sem_give(&data->gpio_sem);
-#elif defined(CONFIG_ICM42688_TRIGGER_GLOBAL_THREAD)
+#elif defined(CONFIG_ICM4268X_TRIGGER_GLOBAL_THREAD)
 	k_work_submit(&data->work);
 #endif
-	if (IS_ENABLED(CONFIG_ICM42688_STREAM)) {
-		icm42688_fifo_event(data->dev);
+	if (IS_ENABLED(CONFIG_ICM4268X_STREAM)) {
+		icm4268x_fifo_event(data->dev);
 	}
 }
 
-#if defined(CONFIG_ICM42688_TRIGGER_OWN_THREAD) || defined(CONFIG_ICM42688_TRIGGER_GLOBAL_THREAD)
-static void icm42688_thread_cb(const struct device *dev)
+#if defined(CONFIG_ICM4268X_TRIGGER_OWN_THREAD) || defined(CONFIG_ICM4268X_TRIGGER_GLOBAL_THREAD)
+static void icm4268x_thread_cb(const struct device *dev)
 {
-	struct icm42688_dev_data *data = dev->data;
+	struct icm4268x_dev_data *data = dev->data;
 
-	icm42688_lock(dev);
+	icm4268x_lock(dev);
 
 	if (data->data_ready_handler != NULL) {
 		data->data_ready_handler(dev, data->data_ready_trigger);
 	}
 
-	icm42688_unlock(dev);
+	icm4268x_unlock(dev);
 }
 #endif
 
-#ifdef CONFIG_ICM42688_TRIGGER_OWN_THREAD
+#ifdef CONFIG_ICM4268X_TRIGGER_OWN_THREAD
 
-static void icm42688_thread(void *p1, void *p2, void *p3)
+static void icm4268x_thread(void *p1, void *p2, void *p3)
 {
 	ARG_UNUSED(p2);
 	ARG_UNUSED(p3);
 
-	struct icm42688_dev_data *data = p1;
+	struct icm4268x_dev_data *data = p1;
 
 	while (1) {
 		k_sem_take(&data->gpio_sem, K_FOREVER);
-		icm42688_thread_cb(data->dev);
+		icm4268x_thread_cb(data->dev);
 	}
 }
 
-#elif defined(CONFIG_ICM42688_TRIGGER_GLOBAL_THREAD)
+#elif defined(CONFIG_ICM4268X_TRIGGER_GLOBAL_THREAD)
 
-static void icm42688_work_handler(struct k_work *work)
+static void icm4268x_work_handler(struct k_work *work)
 {
-	struct icm42688_dev_data *data = CONTAINER_OF(work, struct icm42688_dev_data, work);
+	struct icm4268x_dev_data *data = CONTAINER_OF(work, struct icm4268x_dev_data, work);
 
-	icm42688_thread_cb(data->dev);
+	icm4268x_thread_cb(data->dev);
 }
 
 #endif
 
-int icm42688_trigger_set(const struct device *dev, const struct sensor_trigger *trig,
+int icm4268x_trigger_set(const struct device *dev, const struct sensor_trigger *trig,
 			 sensor_trigger_handler_t handler)
 {
-	struct icm42688_dev_data *data = dev->data;
-	const struct icm42688_dev_cfg *cfg = dev->config;
+	struct icm4268x_dev_data *data = dev->data;
+	const struct icm4268x_dev_cfg *cfg = dev->config;
 	uint8_t status;
 	int res = 0;
 
@@ -89,7 +90,7 @@ int icm42688_trigger_set(const struct device *dev, const struct sensor_trigger *
 		return -EINVAL;
 	}
 
-	icm42688_lock(dev);
+	icm4268x_lock(dev);
 	gpio_pin_interrupt_configure_dt(&cfg->gpio_int1, GPIO_INT_DISABLE);
 
 	switch (trig->type) {
@@ -99,23 +100,23 @@ int icm42688_trigger_set(const struct device *dev, const struct sensor_trigger *
 		data->data_ready_handler = handler;
 		data->data_ready_trigger = trig;
 
-		res = icm42688_spi_read(&cfg->spi, REG_INT_STATUS, &status, 1);
+		res = icm4268x_spi_read(&cfg->spi, REG_INT_STATUS, &status, 1);
 		break;
 	default:
 		res = -ENOTSUP;
 		break;
 	}
 
-	icm42688_unlock(dev);
+	icm4268x_unlock(dev);
 	gpio_pin_interrupt_configure_dt(&cfg->gpio_int1, GPIO_INT_EDGE_TO_ACTIVE);
 
 	return res;
 }
 
-int icm42688_trigger_init(const struct device *dev)
+int icm4268x_trigger_init(const struct device *dev)
 {
-	struct icm42688_dev_data *data = dev->data;
-	const struct icm42688_dev_cfg *cfg = dev->config;
+	struct icm4268x_dev_data *data = dev->data;
+	const struct icm4268x_dev_cfg *cfg = dev->config;
 	int res = 0;
 
 	if (!cfg->gpio_int1.port) {
@@ -130,7 +131,7 @@ int icm42688_trigger_init(const struct device *dev)
 
 	data->dev = dev;
 	gpio_pin_configure_dt(&cfg->gpio_int1, GPIO_INPUT);
-	gpio_init_callback(&data->gpio_cb, icm42688_gpio_callback, BIT(cfg->gpio_int1.pin));
+	gpio_init_callback(&data->gpio_cb, icm4268x_gpio_callback, BIT(cfg->gpio_int1.pin));
 	res = gpio_add_callback(cfg->gpio_int1.port, &data->gpio_cb);
 
 	if (res < 0) {
@@ -139,21 +140,21 @@ int icm42688_trigger_init(const struct device *dev)
 	}
 
 	k_mutex_init(&data->mutex);
-#if defined(CONFIG_ICM42688_TRIGGER_OWN_THREAD)
+#if defined(CONFIG_ICM4268X_TRIGGER_OWN_THREAD)
 	k_sem_init(&data->gpio_sem, 0, K_SEM_MAX_LIMIT);
-	k_thread_create(&data->thread, data->thread_stack, CONFIG_ICM42688_THREAD_STACK_SIZE,
-			icm42688_thread, data, NULL, NULL,
-			K_PRIO_COOP(CONFIG_ICM42688_THREAD_PRIORITY), 0, K_NO_WAIT);
-#elif defined(CONFIG_ICM42688_TRIGGER_GLOBAL_THREAD)
-	data->work.handler = icm42688_work_handler;
+	k_thread_create(&data->thread, data->thread_stack, CONFIG_ICM4268X_THREAD_STACK_SIZE,
+			icm4268x_thread, data, NULL, NULL,
+			K_PRIO_COOP(CONFIG_ICM4268X_THREAD_PRIORITY), 0, K_NO_WAIT);
+#elif defined(CONFIG_ICM4268X_TRIGGER_GLOBAL_THREAD)
+	data->work.handler = icm4268x_work_handler;
 #endif
 	return gpio_pin_interrupt_configure_dt(&cfg->gpio_int1, GPIO_INT_EDGE_TO_ACTIVE);
 }
 
-int icm42688_trigger_enable_interrupt(const struct device *dev, struct icm42688_cfg *new_cfg)
+int icm4268x_trigger_enable_interrupt(const struct device *dev, struct icm4268x_cfg *new_cfg)
 {
 	int res;
-	const struct icm42688_dev_cfg *cfg = dev->config;
+	const struct icm4268x_dev_cfg *cfg = dev->config;
 
 	/* pulse-mode (auto clearing), push-pull and active-high */
 	res = icm42688_spi_single_write(&cfg->spi, REG_INT_CONFIG,
@@ -163,7 +164,7 @@ int icm42688_trigger_enable_interrupt(const struct device *dev, struct icm42688_
 	}
 
 	/* Deassert async reset for proper INT pin operation, see datasheet 14.50 */
-	res = icm42688_spi_single_write(&cfg->spi, REG_INT_CONFIG1, 0);
+	res = icm4268x_spi_single_write(&cfg->spi, REG_INT_CONFIG1, 0);
 	if (res != 0) {
 		return res;
 	}
@@ -180,19 +181,19 @@ int icm42688_trigger_enable_interrupt(const struct device *dev, struct icm42688_
 	if (new_cfg->interrupt1_fifo_full) {
 		value |= FIELD_PREP(BIT_FIFO_FULL_INT1_EN, 1);
 	}
-	return icm42688_spi_single_write(&cfg->spi, REG_INT_SOURCE0, value);
+	return icm4268x_spi_single_write(&cfg->spi, REG_INT_SOURCE0, value);
 }
 
-void icm42688_lock(const struct device *dev)
+void icm4268x_lock(const struct device *dev)
 {
-	struct icm42688_dev_data *data = dev->data;
+	struct icm4268x_dev_data *data = dev->data;
 
 	k_mutex_lock(&data->mutex, K_FOREVER);
 }
 
-void icm42688_unlock(const struct device *dev)
+void icm4268x_unlock(const struct device *dev)
 {
-	struct icm42688_dev_data *data = dev->data;
+	struct icm4268x_dev_data *data = dev->data;
 
 	k_mutex_unlock(&data->mutex);
 }
