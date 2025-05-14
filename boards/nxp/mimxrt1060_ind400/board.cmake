c63302be93f11a0a8b2413ede0cd12d8f54cd6d0
Xiaoli Ji <xiaoli.ji@nxp.com>
2025-05-14 16:39:19 +0800

    boards: nxp: add IND400 boards and hyperram test
    Signed-off-by: Xiaoli Ji <xiaoli.ji@nxp.com>

---
+++ boards/nxp/mimxrt1060_ind400/board.cmake
@@ -0,0 +1,15 @@
+#
+# Copyright (c) 2018, 2023 NXP
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+board_runner_args(pyocd "--target=mimxrt1060")
+board_runner_args(jlink "--device=MIMXRT1062xxx6A")
+
+board_runner_args(linkserver  "--device=MIMXRT1062xxxxA:EVK-MIMXRT1060")
+
+board_runner_args(jlink "--loader=BankAddr=0x60000000&Loader=QSPI")
+
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/linkserver.board.cmake)
--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -6,9 +6,41 @@
 
 #include <stdio.h>
 
+int test_Hpram_space(void)
+{
+    /* test ram space. */
+    uint32_t *hyperram_start = (uint32_t *)0x70000000;
+    uint32_t hyperram_size   = 0x02000000/4;  //32M
+    uint32_t i;
+    uint32_t temp;
+
+    for(i = 0; i < hyperram_size; ++i)
+    {
+        hyperram_start[i] = i;
+    }
+    for(i = 0; i < hyperram_size; ++i)
+    {
+        temp = hyperram_start[i];
+        if(i != temp)
+        {
+            printf("WRONG HAPPENED! address:0x%08x, value:0x%08x, i:0x%08x\r\n", &hyperram_start[i], temp, i);
+            return -1;
+        }
+    }
+
+    return 0;
+}
+
 int main(void)
 {
 	printf("Hello World! %s\n", CONFIG_BOARD_TARGET);
+
+	uint32_t status = test_Hpram_space();
+
+	if (status == 0)
+	{
+		printf("hyperram test success");
+	}
 
 	return 0;
 }
