0cd79d524ba250b23405f13c5befa22e6e62ecea
Ha Duong Quang <ha.duongquang@nxp.com>
2025-06-11 14:24:16 +0700

    soc: arm: introduce support for NXP S32K566 SoC
    S32K566 is a member of the S32K5 family which expands
    s32k3 series to higher performance and larger memory.
    
    Zephyr port for S32K5 will support cortex-M7 and cortex-R52
    
    After reset, swt_startup is enabled and starts running,
    disable it using the watchdog hook.
    
    Signed-off-by: Ha Duong Quang <ha.duongquang@nxp.com>
    Co-authored-by: Dat Nguyen Duy <dat.nguyenduy@nxp.com>

---
+++ soc/nxp/s32/s32k5/r52/soc.c
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2025 NXP
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/cache.h>
+
+#include <OsIf.h>
+
+void soc_early_init_hook(void)
+{
+	sys_cache_instr_enable();
+	sys_cache_data_enable();
+
+	OsIf_Init(NULL);
+}
