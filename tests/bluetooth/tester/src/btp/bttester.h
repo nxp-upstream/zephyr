0bd29e298774d030f5290bea19540c17b13512fc
Cheng Chang <cheng.chang@nxp.com>
2025-11-27 16:52:22 +0800

    test: Bluetooth: Tester: Add support for A2DP
    This adds support for BTP commands and events required for running
    A2DP tests.
    
    The main changes are as follows:
    1. Add source code for a2dp tests in btp_a2dp.h and btp_a2dp.c.
    2. Add a2dp to the BTP system.
    
    Signed-off-by: Cheng Chang <cheng.chang@nxp.com>

--- tests/bluetooth/tester/src/btp/bttester.h
+++ tests/bluetooth/tester/src/btp/bttester.h
@@ -157,3 +157,6 @@ uint8_t tester_unregister_hfp_hf(void);
 
 uint8_t tester_init_hfp_ag(void);
 uint8_t tester_unregister_hfp_ag(void);
+
+uint8_t tester_init_a2dp(void);
+uint8_t tester_unregister_a2dp(void);
