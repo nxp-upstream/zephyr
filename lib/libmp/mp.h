169a0660cc10f1409ca2890c3ef98007b8aa5d89
Phi Bang Nguyen <phibang.nguyen@nxp.com>
2025-12-01 17:15:07 +0100

    rfc: Drop mp_pixel_format
    Drop mp_pixel_format from RFC to be easy to rebase
    
    Signed-off-by: Phi Bang Nguyen <phibang.nguyen@nxp.com>

--- lib/libmp/mp.h
+++ lib/libmp/mp.h
@@ -20,9 +20,6 @@
 #include <src/core/mp_element.h>
 #include <src/core/mp_element_factory.h>
 #include <src/core/mp_pipeline.h>
-#if (CONFIG_MP_PLUGIN_ZVID || CONFIG_MP_PLUGIN_ZDISP)
-#include <src/core/mp_pixel_format.h>
-#endif
 
 #if CONFIG_MP_PLUGIN_ZVID
 #include <src/plugins/zvid/mp_zvid_property.h>
