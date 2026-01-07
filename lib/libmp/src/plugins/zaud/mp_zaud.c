2c2341df18cc95f9aa5bee78fb5d3d7a038f7b69
Phi Bang Nguyen <phibang.nguyen@nxp.com>
2026-01-07 15:31:36 +0100

    fixup! lib: libMP: Add zaud plugin - Rework pipeline creation
    Signed-off-by: Phi Bang Nguyen <phibang.nguyen@nxp.com>

--- lib/libmp/src/plugins/zaud/mp_zaud.c
+++ lib/libmp/src/plugins/zaud/mp_zaud.c
@@ -6,13 +6,7 @@
 
 #include <zephyr/audio/audio_caps.h>
 
-#include <src/core/mp_element_factory.h>
-#include <src/core/mp_plugin.h>
-
 #include "mp_zaud.h"
-#include "mp_zaud_dmic_src.h"
-#include "mp_zaud_gain.h"
-#include "mp_zaud_i2s_codec_sink.h"
 
 struct mp_zaud_desc {
 	uint32_t value;
@@ -55,16 +49,3 @@ const uint32_t audio2mp_bit_width(uint32_t bit_width_mask)
 
 	return 0;
 }
-
-static void plugin_init(void)
-{
-	MP_ELEMENT_FACTORY_DEFINE(MP_ZAUD_DMIC_SRC_ELEM, sizeof(struct mp_zaud_dmic_src),
-				  mp_zaud_dmic_src_init);
-	MP_ELEMENT_FACTORY_DEFINE(MP_ZAUD_GAIN_ELEM, sizeof(struct mp_zaud_gain),
-				  mp_zaud_gain_init);
-	MP_ELEMENT_FACTORY_DEFINE(MP_ZAUD_I2S_CODEC_SINK_ELEM,
-				  sizeof(struct mp_zaud_i2s_codec_sink),
-				  mp_zaud_i2s_codec_sink_init);
-}
-
-MP_PLUGIN_DEFINE(MP_ZAUD_PLUGIN, plugin_init);
