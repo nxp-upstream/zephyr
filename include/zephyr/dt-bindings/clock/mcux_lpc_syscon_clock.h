963037e7fe0bde7bf820f54520aa9463ef4391fa
Yves Wang <zhengjia.wang@nxp.com>
2026-01-27 15:58:34 +0800

    drivers: clock_control: mcux_syscon: partial support WWDT clock control
    Add clock control support for MCUX WWDT instances (wwdt0 and wwdt1)
    in the MCUX SYSCON clock control driver. This enables proper clock
    gating and frequency retrieval for WWDT peripherals across multiple
    NXP SoC families.
    
    The patch add support for turn on WWDT clock and get it's rate.
    
    Signed-off-by: Yves Wang <zhengjia.wang@nxp.com>

--- include/zephyr/dt-bindings/clock/mcux_lpc_syscon_clock.h
+++ include/zephyr/dt-bindings/clock/mcux_lpc_syscon_clock.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020-2025 NXP
+ * Copyright 2020-2026 NXP
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -158,5 +158,10 @@
 
 /** SEMA42 peripheral clock identifier. */
 #define MCUX_SEMA42_CLK MCUX_LPC_CLK_ID(0x23, 0x00)
+
+/** WWDT0 peripheral clock identifier. */
+#define MCUX_WWDT0_CLK MCUX_LPC_CLK_ID(0x24, 0x00)
+/** WWDT1 peripheral clock identifier. */
+#define MCUX_WWDT1_CLK MCUX_LPC_CLK_ID(0x24, 0x01)
 
 #endif /* ZEPHYR_INCLUDE_DT_BINDINGS_CLOCK_MCUX_LPC_SYSCON_H_ */
