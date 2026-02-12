efd5bd7350922cb62955c99251f0efa18519d944
Dv Alan <zhangyang.shen@nxp.com>
2026-02-12 11:33:43 +0800

    usb: host: ch9: Add string descriptor related request helper function
    Add 4 functions to handle USB string descriptor operations:
    - usbh_req_desc_str() to retrieve USB string descriptors from device
    - usbh_desc_is_valid_string() to validate string descriptor type
    - usbh_desc_get_supported_langs() to get supported languages list
    - usbh_desc_str_utfle16_to_ascii() to convert UTF-16LE string to ASCII
    
    Signed-off-by: Dv Alan <zhangyang.shen@nxp.com>

--- subsys/usb/host/usbh_ch9.c
+++ subsys/usb/host/usbh_ch9.c
@@ -180,6 +180,20 @@ int usbh_req_desc_cfg(struct usb_device *const udev,
 	return ret;
 }
 
+int usbh_req_desc_str(struct usb_device *const udev,
+		      const uint8_t index, const uint16_t lang_id,
+		      struct net_buf *const desc_buf)
+{
+	uint16_t len;
+
+	len = net_buf_tailroom(desc_buf);
+	if (len > UINT8_MAX) {
+		len = UINT8_MAX;
+	}
+
+	return usbh_req_desc(udev, USB_DESC_STRING, index, lang_id, len, desc_buf);
+}
+
 int usbh_req_set_address(struct usb_device *const udev,
 			 const uint8_t addr)
 {
