5fbee9ae755fbceecc0cbac3988392737be91a1c
Kate Wang <yumeng.wang@nxp.com>
2026-02-28 18:57:28 +0800

    drivers: display: Add pixel formats to align with video formats
    Add support for additional pixel formats, so that the display pixel
    formats are aligned with the video pixel formats:
    - PIXEL_FORMAT_BGR_888: 24-bit BGR format with 8 bits per component
    - PIXEL_FORMAT_XRGB_8888: 32-bit RGB with 8 bits padding
    - PIXEL_FORMAT_ABGR_8888: 32-bit ABGR with alpha channel
    - PIXEL_FORMAT_RGBA_8888: 32-bit RGBA with alpha channel
    - PIXEL_FORMAT_BGRA_8888: 32-bit BGRA with alpha channel
    
    Signed-off-by: Kate Wang <yumeng.wang@nxp.com>

--- include/zephyr/drivers/display.h
+++ include/zephyr/drivers/display.h
@@ -145,10 +145,80 @@ enum display_pixel_format {
 	PIXEL_FORMAT_AL_88		= BIT(7), /**< 8-bit Grayscale/Luminance with alpha */
 
 	/**
+	 * 24-bit BGR format with 8 bits per component.
+	 *
+	 * Below shows how data are organized in memory.
+	 *
+	 * @code{.unparsed}
+	 *   Byte 0   Byte 1   Byte 2
+	 *   7......0 15.....8 23....16
+	 * | Rrrrrrrr Gggggggg Bbbbbbbb | ...
+	 * @endcode
+	 *
+	 */
+	PIXEL_FORMAT_BGR_888		= BIT(8), /**< 24-bit BGR */
+
+	/**
+	 * 32-bit RGB format with 8 bits per component and 8 bits for padding.
+	 *
+	 * Below shows how data are organized in memory.
+	 *
+	 * @code{.unparsed}
+	 *   Byte 0   Byte 1   Byte 2   Byte 3
+	 *   7......0 15.....8 23....16 31....24
+	 * | Bbbbbbbb Gggggggg Rrrrrrrr Xxxxxxxx | ...
+	 * @endcode
+	 *
+	 */
+	PIXEL_FORMAT_XRGB_8888		= BIT(9), /**< 32-bit XRGB */
+
+	/**
+	 * 32-bit BGR format with 8 bits per component and 8 bits for alpha.
+	 *
+	 * Below shows how data are organized in memory.
+	 *
+	 * @code{.unparsed}
+	 *   Byte 0   Byte 1   Byte 2   Byte 3
+	 *   7......0 15.....8 23....16 31....24
+	 * | Aaaaaaaa Rrrrrrrr Gggggggg Bbbbbbbb | ...
+	 * @endcode
+	 *
+	 */
+	PIXEL_FORMAT_ABGR_8888		= BIT(10), /**< 32-bit ABGR */
+
+	/**
+	 * 32-bit RGB format with 8 bits per component and 8 bits for alpha.
+	 *
+	 * Below shows how data are organized in memory.
+	 *
+	 * @code{.unparsed}
+	 *   Byte 0   Byte 1   Byte 2   Byte 3
+	 *   7......0 15.....8 23....16 31....24
+	 * | Aaaaaaaa Bbbbbbbb Gggggggg Rrrrrrrr | ...
+	 * @endcode
+	 *
+	 */
+	PIXEL_FORMAT_RGBA_8888		= BIT(11), /**< 32-bit RGBA */
+
+	/**
+	 * 32-bit BGR format with 8 bits per component and 8 bits for alpha.
+	 *
+	 * Below shows how data are organized in memory.
+	 *
+	 * @code{.unparsed}
+	 *   Byte 0   Byte 1   Byte 2   Byte 3
+	 *   7......0 15.....8 23....16 31....24
+	 * | Rrrrrrrr Gggggggg Bbbbbbbb Aaaaaaaa | ...
+	 * @endcode
+	 *
+	 */
+	PIXEL_FORMAT_BGRA_8888		= BIT(12), /**< 32-bit BGRA */
+
+	/**
 	 * This and higher values are display specific.
 	 * Refer to the display header file.
 	 */
-	PIXEL_FORMAT_PRIV_START = (PIXEL_FORMAT_AL_88 << 1),
+	PIXEL_FORMAT_PRIV_START = (PIXEL_FORMAT_BGRA_8888 << 1),
 };
 
 /**
@@ -167,7 +237,12 @@ enum display_pixel_format {
 	(((fmt & PIXEL_FORMAT_RGB_565) >> 4) * 16U) +				\
 	(((fmt & PIXEL_FORMAT_RGB_565X) >> 5) * 16U) +				\
 	(((fmt & PIXEL_FORMAT_L_8) >> 6) * 8U) +				\
-	(((fmt & PIXEL_FORMAT_AL_88) >> 7) * 16U))
+	(((fmt & PIXEL_FORMAT_AL_88) >> 7) * 16U) +				\
+	(((fmt & PIXEL_FORMAT_BGR_888) >> 8) * 24U) +				\
+	(((fmt & PIXEL_FORMAT_XRGB_8888) >> 9) * 32U) +				\
+	(((fmt & PIXEL_FORMAT_ABGR_8888) >> 10) * 32U) +			\
+	(((fmt & PIXEL_FORMAT_RGBA_8888) >> 11) * 32U) +			\
+	(((fmt & PIXEL_FORMAT_BGRA_8888) >> 12) * 32U))
 
 /**
  * @brief Display screen information
