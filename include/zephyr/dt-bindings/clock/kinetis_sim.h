cb6dc1f9562ec31a6d5e3d77082d293fe19f7062
Holt Sun <holt.sun@nxp.com>
2026-03-05 16:27:55 +0800

    dt-bindings: clock: kinetis_sim: encode specifiers
    Some SIM clock consumers historically pass only the 'name' cell from a
    3-cell <name offset bits> clock specifier. Add helper macros to encode
    gate offset/bit into the name cell and provide KINETIS_SIM_CLOCK_SPEC()
    for devicetree use.
    
    The legacy KINETIS_SIM_*_CLK defines are kept for now and will be
    removed once all in-tree devicetrees have been converted.
    
    Signed-off-by: Holt Sun <holt.sun@nxp.com>

--- include/zephyr/dt-bindings/clock/kinetis_sim.h
+++ include/zephyr/dt-bindings/clock/kinetis_sim.h
@@ -1,6 +1,5 @@
 /*
  * Copyright (c) 2017, 2025 NXP
- * Copyright (c) 2017, 2025 NXP
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -13,8 +12,8 @@
 #define KINETIS_SIM_BUS_CLK		2
 #define KINETIS_SIM_FAST_PERIPHERAL_CLK	5
 #define KINETIS_SIM_LPO_CLK		19
-#define KINETIS_SIM_DMAMUX_CLK  KINETIS_SIM_BUS_CLK
-#define KINETIS_SIM_DMA_CLK     KINETIS_SIM_CORESYS_CLK
+#define KINETIS_SIM_DMAMUX_CLK	KINETIS_SIM_BUS_CLK
+#define KINETIS_SIM_DMA_CLK	KINETIS_SIM_CORESYS_CLK
 #define KINETIS_SIM_SIM_SOPT7   7
 #define KINETIS_SIM_OSCERCLK    8
 #define KINETIS_SIM_MCGIRCLK    12
@@ -33,5 +32,49 @@
 
 #define KINETIS_SIM_CMP_CLK		4323
 #define KINETIS_SIM_VREF_CLK		4324
+
+/*
+ * Kinetis SIM clock specifier encoding for consumers that only pass the `name`
+ * cell to the clock_control API.
+ *
+ * Some devicetrees provide a 3-cell SIM clock specifier:
+ *
+ *   <name offset bits>
+ *
+ * but many drivers historically pass only the `name` cell (via
+ * DT_*_CLOCKS_CELL(..., name)). To allow those drivers to control SIM clock
+ * gates and query rates without per-driver changes, the `name` cell can be
+ * encoded with both gate and rate information.
+ *
+ * Layout (32-bit):
+ *   bit 31      : 1 (encoded marker)
+ *   bits 26..30 : gate bit (0..31)
+ *   bits 13..25 : gate register offset (0..0x1FFF)
+ *   bits 0..12  : clock name selector (0..0x1FFF)
+ */
+
+#define KINETIS_SIM_SUBSYS_ENCODE(name, offset, bits) \
+	(0x80000000U | (((bits) & 0x1FU) << 26) | (((offset) & 0x1FFFU) << 13) | \
+	 ((name) & 0x1FFFU))
+
+#define KINETIS_SIM_SUBSYS_IS_ENCODED(val) \
+	(((val) & 0x80000000U) != 0U)
+
+#define KINETIS_SIM_SUBSYS_DECODE_NAME(val) \
+	((val) & 0x1FFFU)
+
+#define KINETIS_SIM_SUBSYS_DECODE_OFFSET(val) \
+	(((val) >> 13) & 0x1FFFU)
+
+#define KINETIS_SIM_SUBSYS_DECODE_BITS(val) \
+	(((val) >> 26) & 0x1FU)
+
+/*
+ * Convenience for keeping SIM clock specifiers readable:
+ * expands to the required 3-cell <name offset bits> form while only spelling
+ * (offset,bits) once at the call site.
+ */
+#define KINETIS_SIM_CLOCK_SPEC(name, offset, bits) \
+	KINETIS_SIM_SUBSYS_ENCODE(name, offset, bits) offset bits
 
 #endif /* ZEPHYR_INCLUDE_DT_BINDINGS_CLOCK_KINETIS_SIM_H_ */
