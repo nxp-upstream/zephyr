bf8a09062c04bd545f7eb4d92d985477fbd5e229
Qiang Zhao <qiang.zhao@nxp.com>
2026-07-28 11:14:57 +0800

    boards: nxp: imx952_evk: add SPSDK boot image support for M7
    Add CMakeLists.txt and board.cmake to support building and flashing
    M7 images for the i.MX 952 EVK board using SPSDK (nxpimage).
    
    The boot firmware blob is fetched from:
    imx952-evk-boot-firmware-6.18.20-2.0.0.bin
    
    Signed-off-by: Qiang Zhao <qiang.zhao@nxp.com>

--- boards/nxp/imx952_evk/board.cmake
+++ boards/nxp/imx952_evk/board.cmake
@@ -1,7 +1,18 @@
 # SPDX-License-Identifier: Apache-2.0
 
+if(CONFIG_BOARD_NXP_SPSDK_IMAGE OR (DEFINED ENV{USE_NXP_SPSDK_IMAGE}
+  AND "$ENV{USE_NXP_SPSDK_IMAGE}" STREQUAL "y"))
+  board_set_flasher_ifnset(spsdk)
+
+  board_runner_args(spsdk "--family=mx952")
+  board_runner_args(spsdk "--bootloader=${CMAKE_BINARY_DIR}/zephyr/imx952-evk-boot-firmware-6.18.20-2.0.0/imx-boot-imx952-19x19-lpddr5-evk-sd.bin-flash_all")
+  board_runner_args(spsdk "--flashbin=${CMAKE_BINARY_DIR}/zephyr/flash.bin")
+  board_runner_args(spsdk "--containers=one")
+
+  include(${ZEPHYR_BASE}/boards/common/spsdk.board.cmake)
+endif()
+
 if(CONFIG_SOC_MIMX9529_A55)
   board_runner_args(jlink "--device=IMX952_A55" "--no-reset" "--flash-sram")
-
   include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
 endif()
